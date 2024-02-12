9709b6e25ff0fa1a3d619c78753ec90ca4ef755f
Lautaro Galante <lautaro9galante@gmail.com>
2024-02-12 20:32:19 -0300

    update resquest_data

--- server/service/request_data.go
+++ server/service/request_data.go
@@ -1,6 +1,8 @@
 package service
 
 type ClientData struct {
-	DirectoryName string `json:"directory"`
-	Path          string `json:"path"`
+	DirectoryName string   `json:"directory"`
+	Path          string   `json:"path"`
+	Files         []string `json:"files"`
+	File          string   `json:"file"`
 }
