77f9dce2b14db1c2fbf69a78cf378ceed26302fa
Lautaro Galante <lautaro9galante@gmail.com>
2024-02-17 04:18:43 -0300

    Adding buttons

--- client/src/components/Aside-bar.tsx
+++ client/src/components/Aside-bar.tsx
@@ -1,9 +1,21 @@
 import '../styles/AsideBar.css'
+import '../components/Button'
+import Button from '../components/Button';
 export default function LeftBar() {
-
+    
     return (
         <div className="left-bar">
-            <p>left bar</p>
+            <div className="logo-container">
+                <span>My Cloud</span> 
+            </div>
+            <div className="button-container">
+                <div className="upload-button-container">
+                    <Button icon= 'fa-solid fa-plus' title='Nuevo' type='upload'/>
+                </div>
+                <div className="mkdir-button-container">
+                    <Button icon='fa-solid fa-folder-plus' title='Crear carpeta' type='mkdir'/>
+                </div>
+            </div>
         </div>
     );
 
