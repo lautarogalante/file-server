d5cc1d04929fb8860baff35e7aa7f9f5cb43084d
Lautaro Galante <lautaro9galante@gmail.com>
2024-02-17 04:21:35 -0300

    Create Button component

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/Button.module.css'
+interface ButtonOptions {
+    title: string;
+    icon?: string;
+    type?: string;
+    onClick?: () => void;
+}
+
+
+export default function Button(btn: ButtonOptions) {
+    const buttonClass = `${styles.button} ${btn.type ? styles[btn.type] : 'button'}`;
+    return (
+        <button className={buttonClass}><span className={styles.button__icon}><i className={btn.icon}></i></span> 
+            <span className={styles.button__text}>{btn.title}</span>
+        </button>
+    );
+
+}
