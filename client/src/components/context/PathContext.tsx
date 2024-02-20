c2caab7e5047fa3e389f22c2c7f04a3c49d42021
Lautaro Galante <lautaro9galante@gmail.com>
2024-02-19 23:22:29 -0300

    Creating PathContext

---
+++ client/src/components/context/PathContext.tsx
@@ -0,0 +1,18 @@
+import {createContext } from "react";
+
+
+ type PathContextType  = {
+    pathFlag: boolean;
+    pathValue: string;
+    changePathValue: (newPath: string) => void;
+    changePathFlag: () => void;
+ };
+
+export const PathContext = createContext<PathContextType>({
+    pathFlag: false,
+    pathValue: "/home/lautaro",
+    changePathValue: () => {},
+    changePathFlag: () => {}
+});
+
+
