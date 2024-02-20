a3885e15a720cf44010d1aa581e5876d05607ccd
Lautaro Galante <lautaro9galante@gmail.com>
2024-02-19 23:20:12 -0300

    adding PathProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,14 @@
 import './styles/App.css'
 import './styles/DataSection.css'
 import './components/AsideBar'
-import LeftBar from './components/AsideBar'
-import TopBar from './components/TopBar'
-import PropertyBar from './components/PropertyBar'
-import DataSection from './components/FileAndDirectory'
-import { useState } from 'react'
-import { OptionsIcons } from './components/OptionsIcons'
+import { PathProvider } from './components/context/PathProvider'
+import MainApp from './components/MainApp'
 
 function App() {
-    const [basePathUpdated, setBasePathUpdated] = useState<boolean>(false);
-    const updateBasePath = () => {
-        setBasePathUpdated(prevState => !prevState);
-    };
-
   return (
-    <div className='main-container'>
-      <div className='left-bar-container'>
-        <LeftBar/>
-      </div>
-      <div className='main-section'>
-        <TopBar icon='fa fa-search'/>
-        <OptionsIcons updateBasePath={updateBasePath}/>
-        <div className='data-section'>
-          <PropertyBar/>
-          <div className="data-container">
-            <DataSection basePathUpdated={basePathUpdated} setBasePathUpdated={setBasePathUpdated}/> 
-          </div>
-        </div> 
-      </div>
-    </div>
+    <PathProvider>
+      <MainApp/>
+    </PathProvider>
   )
 }
 export default App
