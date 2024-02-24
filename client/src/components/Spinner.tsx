a67d46168ab7699fb8b10c18ed166c3868ffdce0
Lautaro Galante <lautaro9galante@gmail.com>
2024-02-24 19:56:38 -0300

    Implementing Spinner component

---
+++ client/src/components/Spinner.tsx
@@ -0,0 +1,14 @@
+import '../styles/Spinner.css'
+
+export const Spinner = () => {
+    return (
+        <>
+            <div className="lds-ring">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </>
+    );
+}
