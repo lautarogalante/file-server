2febb696398c28ed53c4be87f973e2d97114df9b
Lautaro Galante <lautaro9galante@gmail.com>
2024-02-23 21:38:18 -0300

    Cleaning

--- server/service/request_data.go
+++ server/service/request_data.go
@@ -3,11 +3,7 @@ package service
 type RequestData struct {
 	DirectoryName string   `json:"directory"`
 	QueryPath     string   `query:"queryPath"`
+	Target        string   `query:"target"`
 	Path          string   `json:"path"`
 	Files         []string `json:"files"`
-	File          string   `json:"file"`
-}
-
-type SendFiles struct {
-	Path string `json:"path"`
 }
